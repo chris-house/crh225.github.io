c9dbe29a127532b2361c6a05c7b4dbd477bbf15f
CH <chris@chrishouse.us>
2018-05-28 18:13:02 -0500

    (chore): testing deployment

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -18,6 +18,7 @@ class Counter extends React.Component {
                 } ) }
                 >plus
         </button>
+                <br />
                 <button onClick={ () => this.setState( {
                     count: this.state.count -
                         1
@@ -25,7 +26,7 @@ class Counter extends React.Component {
                 >minus
         </button>
                 <br />
-                    <Link to="/">Go back to the homepage</Link>                      
+                <Link to="/">Go back to the homepage</Link>
             </div>
         )
     }
