e06b149185c649375842226f8e8379d7faa82abf
CH <chris@chrishouse.us>
2018-05-28 18:04:50 -0500

    (feat): initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Chris House\'s Blog',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    'gatsby-plugin-react-helmet'
+  ]
+ 
+}
